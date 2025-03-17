a201ac8b3c5f14a6045e31ddc51d864c15a43690
Mikhail <micheliangello777@gmail.com>
2025-03-18 07:57:25 +0900

    fix gosec errors

--- json.go
+++ json.go
@@ -29,6 +29,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		log.Fatalf("Cannot write %v", err)
+		log.Printf("Crititcal error writing response: %v", err)
 	}
 }
